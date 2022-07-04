b47822bead9f54a3e70d1ae04ce8039a39be93bd
tejalsakore <sakoretejal@1511.com>
2022-07-04 18:17:04 +0530

    Practiced Ternary Operator

---
+++ ternary_operator.js
@@ -0,0 +1,7 @@
+// Show signout button if authenticated
+// Show login option if unauthenticated
+
+authenticated = true;
+authenticated ? console.log("Signout Button") : console.log("Login Option");
+
+// Signout Button
