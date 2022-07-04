ce8c3778a4951d8b2b1559f0a8845216959d4147
tejalsakore <sakoretejal@1511.com>
2022-07-04 18:11:18 +0530

    updated operators

---
+++ operators.js
@@ -0,0 +1,26 @@
+
+var num1 =7;
+var num2 =8;
+// Addition Operator
+console.log(num1+num2);    // 15
+
+// Subtraction Operator 
+console.log(num2-num1);    // 1
+
+// Multiplication Operator 
+console.log(num1*num2);    // 56 
+
+// Division Operator 
+console.log(num1/num2);    // 0.875
+
+// Remainder Operator
+console.log(num1%num2);    // 7
+
+// Exponential Operator
+console.log(num1**num2);   // 5764801
+
+// Greater than Operator
+console.log(num1>num2);    // false
+
+// Less than Operator
+console.log(num1<num2);    // true
