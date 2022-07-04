67633b07052e1e7f0118b330212cf62905a933ca
tejalsakore <sakoretejal@1511.com>
2022-07-04 17:18:53 +0530

    Practiced IF-ELSE

---
+++ if_else.js
@@ -0,0 +1,17 @@
+// Conditional Statements
+
+temp = 20;
+if(temp<20)
+{
+    console.log("It's very cold outside");
+}
+if(temp<30)
+{
+    console.log("It's moderate outside");
+}
+else
+{
+    console.log("It's very hot outside");
+}
+
+// It's moderate outside
