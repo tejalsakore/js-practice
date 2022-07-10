54967bc556846ee71e798cf310e91f6bf5465760
tejalsakore <sakoretejal1511@gmail.com>
2022-07-10 18:46:24 +0530

    Practiced Variables

--- variables.js
+++ variables.js
@@ -1,12 +1,14 @@
 // variables are used to reserve memory
 // reserved kwds can't be used as variables
-
+//Example 1
 var name = "Tejal";
 console.log(name);               // Tejal
 
+//Example 2
 var age  = 20;
 console.log(age);                // 20
 
+//Example 3
 var paymentMode;                 // undefined
 paymentMode = "credit card";                                     
 console.log(paymentMode);        // credit card
