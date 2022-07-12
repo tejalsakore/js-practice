76ecb832c9b96dca9d7a8e95bd5cd620de5ae77d
tejalsakore <sakoretejal1511@gmail.com>
2022-07-12 21:28:08 +0530

    Practiced forEach loop

---
+++ forEach_loop.js
@@ -0,0 +1,11 @@
+//For each loop
+var myStates=["Maharashtra","Assam","Goa",2001,"Punjab","Kerala"];
+myStates.forEach((s) => (console.log(s)));
+//or
+myStates.forEach(s => console.log(s));
+/*Maharashtra
+Assam
+Goa
+2001
+Punjab
+Kerala*/
