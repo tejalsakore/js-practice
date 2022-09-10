655bad5a8f8dfdbb60600a85a8e9dce01fd30943
tejalsakore <sakoretejal1511@gmail.com>
2022-09-10 20:01:26 +0530

    added new and this kwd

---
+++ new_and_this_kwd.js
@@ -0,0 +1,18 @@
+var User = function(firstName,age,email){
+    this.FirstName= firstName;
+    this.Age= age;
+    this.Email= email;
+}
+
+// var User1 = User("Tejal", 20, "sakoretejal@gmail.com")
+// console.log(User1)                                       undefined output
+
+var User1 = new User("Tejal", 20, "sakoretejal@gmail.com")
+var User2 = new User("Tanay", 25, "tanay@gmail.com")
+console.log(User1)
+console.log(User2)
+
+/* Output:
+
+User { FirstName: 'Tejal', Age: 20, Email: 'sakoretejal@gmail.com' }
+User { FirstName: 'Tanay', Age: 25, Email: 'tanay@gmail.com' }*/
