43b399ba71747df55ccb4fadf549a762291140ce
tejalsakore <sakoretejal1511@gmail.com>
2022-09-10 20:49:47 +0530

    added proto

---
+++ proto.js
@@ -0,0 +1,20 @@
+var User = function(firstName,age){
+    this.FirstName= firstName;
+    this.Age= age;
+    this.getAge= function(){
+        console.log(`Age is :${this.Age}`);
+    };
+};
+
+User.prototype.getFirstName= function(){
+    console.log(`Your First Name is : ${this.FirstName}`);
+};
+
+var User1 = new User("Tejal", 20);
+User1.getFirstName();
+User1.getAge();
+
+/* Output:
+Your First Name is : Tejal
+Age is :20
+*/
