fc245d6a9846ff6d824dfc221d8ecbbc9463ae6e
tejalsakore <sakoretejal1511@gmail.com>
2022-09-21 18:44:44 +0530

    added template literals

---
+++ template_literals.js
@@ -0,0 +1,24 @@
+var name = "Tejal";
+var fullname = `${name} Sakore`;
+
+console.log(fullname);
+console.log(fullname.charAt(0));
+console.log(fullname.endsWith("e"));
+console.log(fullname.startsWith("T"));
+console.log(fullname.includes("Sak"));
+console.log(fullname.toLocaleUpperCase());
+console.log(fullname.toUpperCase());
+
+/*
+
+
+Tejal Sakore
+T
+true
+true
+true
+TEJAL SAKORE
+TEJAL SAKORE
+
+
+*/
